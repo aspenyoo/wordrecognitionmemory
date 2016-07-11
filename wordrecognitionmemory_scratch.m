dcb822994acff802e9263bb46d1ed1daa5df6357
aspenyoo <aspen.yoo@nyu.edu>
2016-07-11 14:36:36 -0400

    misc changes

--- wordrecognitionmemory_scratch.m
+++ wordrecognitionmemory_scratch.m
@@ -16,3 +16,30 @@ end
 
 hist(nLLVec,15); defaultplot
 std(nLLVec)
+
+%% looking at diff parameters effect on log function
+% july 7, 2016
+
+xx = linspace(-10,10,100);
+a = 1;
+b = 1;
+% c = .05;
+nConf = 20;
+
+% yy = a.*log(xx) + b;
+signxx = sign(xx);
+% yy = a.*log(xx) + b + nConf/2 + 1;
+% yy2 = a.*log(xx.*exp(b)./a) + nConf/2 + 1;
+% plot(xx,yy,'k',xx,yy2,'r--'); defaultplot
+yy = min(max(round(a.*log(abs(xx)) + b + nConf/2 + 1),nConf/2 +1),nConf); % log function shifted up to confidence region
+yy2 = min(max(round(a.*log(abs(xx) + b) + nConf/2 + 1),nConf/2 +1),nConf); % log function shifted up to confidence region
+subplot(1,2,1);
+plot(xx,yy,'k',xx,yy2,'r'); defaultplot
+ylim([11 20])
+
+yy(signxx == -1) = nConf+1 - yy(signxx == -1);
+yy2(signxx == -1) = nConf+1 - yy2(signxx == -1);
+subplot(1,2,2);
+plot(xx,yy,'k',xx,yy2,'r'); defaultplot
+ylim([ 1 20])
+% blahhist = hist(yy,1:nConf)
