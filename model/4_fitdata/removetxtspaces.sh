f673707f1e5ea160d4fb10ef93aba6ed08020a93
aspenyoo <aspen.yoo@nyu.edu>
2016-07-11 14:57:00 -0400

    job to remove spaces

---
+++ model/4_fitdata/removetxtspaces.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=00:04:00
+#PBS -j oe
+#PBS -M aspen.yoo@nyu.edu
+#PBS -l mem=2GB
+#PBS -m abe
+#PBS -N paramfit_wrapper
+
+index=${PBS_ARRAYID}
+job=${PBS_JOBID}
+walltime_lim=${PBS_WALLTIME}
+script_name=${PBS_JOBNAME}
+module purge
+module load matlab
+
+export MATLABPATH=/home/ay963/matlab-scripts
+cat<<EOF | matlab -nodisplay
+addpath('/home/ay963/job-scripts')
+addpath(genpath('/home/ay963/matlab-scripts'))
+
+modelname = 'REM';
+
+for isubj = 1:14;
+	removetxtpaces(modelname,isubj)
+end
+
+EOF
+
+
