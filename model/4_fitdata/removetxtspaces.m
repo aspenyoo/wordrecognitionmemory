00a4e466fc98d3c8f682fb7aa7216a05dede5467
aspenyoo <aspen.yoo@nyu.edu>
2016-07-11 15:03:38 -0400

    removetxtspaces.m

--- model/4_fitdata/removetxtspaces.m
+++ model/4_fitdata/removetxtspaces.m
@@ -1,11 +1,8 @@
-function removetxtspaces(modelname,isubj,optimMethod)
+function removetxtspaces(modelname,isubj,optimMethod,filepath)
 if nargin < 3; optimMethod = 'patternbayes'; end
+if nargin < 4; filepath = 'model/4_fitdata/BPSfits'; end
 
-% modelname = 'FPheurs';
-% isubj = 1;
-% optimMethod = 'patternbayes';
-
-filename = ['paramfit_' optimMethod '_' modelname '_subj' num2str(isubj) '.txt'];
+filename = [filepath 'paramfit_' optimMethod '_' modelname '_subj' num2str(isubj) '.txt'];
 
 % Read the file as cell string line by line
 fid = fopen(filename,'r');
