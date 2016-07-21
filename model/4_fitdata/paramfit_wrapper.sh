2a491abcdbf5061b32e483b186265836c26f1e69
Aspen H. Yoo <aspen.yoo@nyu.edu>
2016-07-21 13:56:00 -0400

    paramfit wrapper

--- model/4_fitdata/paramfit_wrapper.sh
+++ model/4_fitdata/paramfit_wrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=7
+#PBS -l nodes=1:ppn=13
 #PBS -l walltime=48:00:00
 #PBS -j oe
 #PBS -M aspen.yoo@nyu.edu
-#PBS -l mem=15GB
+#PBS -l mem=26GB
 #PBS -m abe
 #PBS -N paramfit_wrapper
 
@@ -20,7 +20,7 @@ addpath(genpath('/home/ay963/matlab-scripts'))
 addpath(genpath('/home/ay963/wordrecognitionmemory'))
 
 modelname = 'REM';
-subjids = 1:7;
+subjids = [1:3 5:14];
 
 cluster_wrapper(modelname, subjids, $index+1)
 
