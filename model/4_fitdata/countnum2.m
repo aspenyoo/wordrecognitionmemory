d6a0a1caea6449d31ab6a489657cb59eb2c4c8d2
Aspen H. Yoo <aspen.yoo@nyu.edu>
2016-07-22 19:53:23 -0400

    nonfixed sigma_mc

---
+++ model/4_fitdata/countnum2.m
@@ -0,0 +1,25 @@
+function count = countnum2(testmodelname,binningfn, isubj,fixparams, truemodelname, optimMethod)
+if nargin < 5; truemodelname = testmodelname; end
+if nargin < 6; optimMethod = 'patternbayes'; end
+
+filename = ['paramfit_' optimMethod '_' testmodelname num2str(binningfn) '_subj' num2str(isubj) '.txt'];
+if isubj > 14;
+    filename = ['modelrecovery_patternbayes_' testmodelname num2str(binningfn) '_' truemodelname 'subj' num2str(isubj) '.txt'];
+end
+
+% ensuring that there are no space in the txt to fuck up the reading of the file
+% removetxtspaces(testmodelname,isubj,optimMethod)
+
+filepath = 'model/4_fitdata/BPSfits/';
+try
+    nFixedParams = size(fixparams,2);
+    data = dlmread([filepath filename]);
+    count = zeros(size(data,1),1);
+    for iparam = 1:nFixedParams;
+        count = count + data(:,fixparams(1,iparam)) == fixparams(2,iparam);
+    end
+    count = sum(count == nFixedParams);
+catch
+    count = 0;
+end
+
