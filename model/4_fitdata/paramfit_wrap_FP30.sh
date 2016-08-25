9664707f40ffac01075981c44610efec785acc81
Aspen H. Yoo <aspen.yoo@nyu.edu>
2016-08-25 16:05:30 -0400

    new joblists

--- model/4_fitdata/paramfit_wrap_FP30.sh
+++ model/4_fitdata/paramfit_wrap_FP30.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=24:00:00
+#PBS -l walltime=12:00:00
 #PBS -j oe
 #PBS -M aspen.yoo@nyu.edu
 #PBS -l mem=4GB
 #PBS -m abe
-#PBS -N REM
+#PBS -N FP30
 
 index=${PBS_ARRAYID}
 job=${PBS_JOBID}
@@ -19,8 +19,8 @@ cat<<EOF | matlab -nodisplay
 addpath(genpath('/home/ay963/matlab-scripts'))
 addpath(genpath('/home/ay963/wordrecognitionmemory'))
 
-modelname = 'REM';
-binningfn = 1;
+modelname = 'FP';
+binningfn = 3;
 memstrengthvar = 0;
 
 if $index > 100;
@@ -28,9 +28,9 @@ if $index > 100;
     isubj = str2double(blah(1:end-2));
     joblistnum = str2double(blah(end-1:end)); 
 end
-joblistfile = []; % will do a different job per person. (if you want same for all, write joblist name)
+joblistfile = 'joblist_08112016.txt'; % will do a different job per person. (if you want same for all, write joblist name)
 
-cluster_wrap2(modelname, binningfn, memstrengthvar, isubj, joblistnum,joblistfile,[1 7; nan 0])
+cluster_wrap2(modelname, binningfn, memstrengthvar, isubj, joblistnum,joblistfile,[1 5; nan 0])
 
 EOF
 
