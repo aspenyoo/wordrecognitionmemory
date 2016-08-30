e01894cd4723201cdc428193d4f0d07919a76861
Aspen H. Yoo <aspen.yoo@nyu.edu>
2016-08-30 13:28:53 -0400

    new joblist

---
+++ model/4_fitdata/paramfit_wrap_FP10.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=12:00:00
+#PBS -j oe
+#PBS -M aspen.yoo@nyu.edu
+#PBS -l mem=4GB
+#PBS -m abe
+#PBS -N FP11
+
+index=${PBS_ARRAYID}
+job=${PBS_JOBID}
+walltime_lim=${PBS_WALLTIME}
+script_name=${PBS_JOBNAME}
+module purge
+module load matlab
+
+export MATLABPATH=/home/ay963/matlab-scripts
+cat<<EOF | matlab -nodisplay
+addpath(genpath('/home/ay963/matlab-scripts'))
+addpath(genpath('/home/ay963/wordrecognitionmemory'))
+
+modelname = 'FP';
+binningfn = 0;
+memstrengthvar = 0;
+
+if $index > 100;
+    blah = num2str($index);
+    isubj = str2double(blah(1:end-2));
+    joblistnum = str2double(blah(end-1:end)); 
+end
+joblistfile = 'joblist_08112016.txt'; % will do a different job per person. (if you want same for all, write joblist name)
+
+cluster_wrap2(modelname, binningfn, memstrengthvar, isubj, joblistnum,joblistfile,[1 4; nan 0])
+
+EOF
+
+
