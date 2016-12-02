957f4a0a99e709750722afe902877a0bd5055e35
Aspen Yoo <ay963@login-0-1.local>
2016-12-02 14:21:02 -0500

    some FP and REM fits

--- model/4_fitdata/fit_FP.sh
+++ model/4_fitdata/fit_FP.sh
@@ -5,7 +5,7 @@
 #PBS -M aspen.yoo@nyu.edu
 #PBS -l mem=4GB
 #PBS -m abe
-#PBS -N REM
+#PBS -N FP
 
 index=${PBS_ARRAYID}
 job=${PBS_JOBID}
@@ -19,7 +19,7 @@ cat<<EOF | matlab -nodisplay
 addpath(genpath('/home/ay963/matlab-scripts'))
 addpath(genpath('/home/ay963/wordrecognitionmemory'))
 
-modelname = 'REM';
+modelname = 'FP';
 
 if $index > 100;
     blah = num2str($index);
