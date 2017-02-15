23e035fbe69dc8a9e225ba624e663e03c3199ba6
Aspen H. Yoo <aspen.yoo@nyu.edu>
2017-02-15 17:14:07 -0500

    oak genera

--- wordrecognitionmemory_scratch.m
+++ wordrecognitionmemory_scratch.m
@@ -917,8 +917,8 @@ plot(paramMat(:,1),paramMat(:,2),'o')
 modelname = 'FP';
 binningfn = 3;
 load('allmodels_fits.mat')
-nSamples = 20;
-nSubjs = 14;
+nSamples = 1;
+nSubj = 14;
 nM = 50;
 
 nLLVec = cell(1,nM);
@@ -930,9 +930,149 @@ for isubj = 1:nSubj;
         iM
         theta = parameterestimates.([modelname num2str(binningfn)]){iM}(isubj,:);
         
-        nLLVec{iM} = nan(nSubjs,nSamples);
+%         nLLVec{iM} = nan(nSubjs,nSamples);
         for isample = 1:nSamples
             [ nLLVec{iM}(isubj,isample) ] = nLL_approx_vectorized( modelname, theta, binningfn, nnew_part, nold_part);
         end
     end
 end
+
+%% doing stuff with the above calculation (previoulsy calculated)
+clear all
+
+% load('FP3_nLLsamples.mat')
+nLLVec = cellfun(@(x) sum(mean(x,2)),nLLVec,'UniformOutput',false);
+
+
+%% calculate IQRs of parameters for each model
+clear
+
+model = 'REM4';
+load(['paramfit_patternbayes_' model '.mat'],'bestFitParam')
+bestFitParam = sort(bestFitParam);
+
+percentile25 = mean(bestFitParam(3:4,:))
+percentile75 = mean(bestFitParam(10:11,:))
+median = mean(bestFitParam(7:8,:))
+% plot?
+
+%% K-L divergence
+%% calculating K-L divergence between model and human data
+clear all
+
+modelVec = [1 5 7];
+subjVec = {'1','3','4','EL','ND'};
+
+nModels = length(modelVec);
+nSubj = length(subjVec);
+
+gamma = 0.577215; % eulers constant
+idx = repmat(1:155,2,1);
+A = 2./(1:2:309);
+G = -gamma - log(2) + cumsum(A);
+G = [0 -gamma-log(2) G(idx(:))];
+
+KLMat = nan(nModels,nSubj);
+for isubj = 1:nSubj;
+    subjid = subjVec{isubj};
+    
+    % load data
+    [data] = concatdata(subjid,'Detection');
+    [Xdet] = conditionSeparator(data);
+    nCond = length(Xdet);
+    
+    nBins = 10;
+    entr = nan(1,nCond);
+    % estimating entropy p*log(p)
+    for icond = 1:nCond;
+        blah = Xdet{icond};
+        
+        % grassberger unbiased approximation
+        nresp1 = sum(blah(blah(:,1)==0,2));
+        ntrials = size(blah(blah(:,1)==0,:),1);
+        entr(icond) = ntrials*(G(ntrials) - 1/ntrials*(nresp1*G(nresp1+1) + (ntrials-nresp1)*G(ntrials-nresp1+1)));
+        
+        %         % biased calculation
+        %         presp_nochange = mean(blah(blah(:,1)==0,2));
+        %         entr(icond) = presp_nochange*log(presp_nochange) + (1-presp_nochange)*log(1-presp_nochange);
+        %         entr(icond) = entr(icond).*size(blah(blah(:,1) == 0,1),1);
+        
+        changetrials = sortrows(abs(blah(blah(:,1)~=0,:)));
+        nchangeTrials = size(changetrials,1);
+        nTrialsperBin = floor(nchangeTrials/nBins);
+        leftover = nchangeTrials - nTrialsperBin*nBins;
+        
+        %         % biased
+        %         presp_quantile = [mean(changetrials(1:nTrialsperBin+leftover,2)) nan(1,nBins-1)];
+        %         for ibin = 2:nBins;
+        %             presp_quantile(ibin) = mean(changetrials((ibin-1)*nTrialsperBin+leftover+1:ibin*nTrialsperBin+leftover,2));
+        %         end
+        %         nTrialsbinVec = [nTrialsperBin+leftover nTrialsperBin*ones(1,nBins-1)];
+        %         nTrialsbinVec((presp_quantile == 0) | (presp_quantile == 1)) = [];
+        %         presp_quantile((presp_quantile == 0) | (presp_quantile == 1)) = [];
+        %
+        %         entr(icond) = entr(icond) + nTrialsbinVec*(presp_quantile.*log(presp_quantile) + (1-presp_quantile).*log(1-presp_quantile))';
+        
+        % using grassberger estimator
+        nTrialsbinVec = [nTrialsperBin+leftover nTrialsperBin*ones(1,nBins-1)];
+        trialscumsum = [0 cumsum(nTrialsbinVec)];
+        for ibin = 1:nBins;
+            ntrials = nTrialsbinVec(ibin);
+            nresp1 = sum(changetrials((trialscumsum(ibin)+1):trialscumsum(ibin+1),2));
+            entrr(ibin) = ntrials*(G(ntrials) - 1/ntrials*(nresp1*G(nresp1+1) + (ntrials-nresp1)*G(ntrials-nresp1+1)));
+        end
+        entr(icond) = - entr(icond) - sum(entrr);
+        
+        %     plot(presp_quantile,'ko'); defaultplot; pause
+    end
+    entr = sum(entr) % mean entropy over conditions
+    
+    for imodel = 1:nModels;
+        model = modelVec(imodel);
+        
+        % load model parameters
+        load(['paramfit_model' num2str(model) '.mat']) % load model
+        bleh = cellfun(@(x) strcmp(x,subjid),subjids, 'UniformOutput',false);
+        subjidx = find(cell2mat(bleh) == 1);
+        theta = bestFitParam(subjidx,:);
+        bias = 0;
+        
+        % calculating log likelihood q*log(p)
+        theta(1:nCond) = log(theta(1:nCond));
+        if model == 5; theta(nCond+1) = log(theta(nCond+1)); end
+        [ll] = loglike(Xdet, model, theta);
+        
+        KLMat(imodel,isubj) = entr-ll;
+    end
+    
+end
+%% seeing which model wins as a function of nTrials (NN data)
+
+modelVec = [1 5 7];
+subjVec = 1:5;
+
+nModels = length(modelVec);
+nSubjs = length(subjVec);
+
+AICMat = nan(nModels,nSubjs,10);
+nParamsVec = nan(1,nModels);
+count = zeros(10,3);
+for isubj = 1:nSubjs;
+    subjnum = subjVec(isubj);
+    
+    subjids = {'1','2','3','4','5','6','7','8','9','10'};
+    subjids = cellfun(@(x) [ num2str(subjnum) '_' x '0000'],subjids,'UniformOutput',false);
+    
+    for imodel = 1:nModels;
+        model = modelVec(imodel);
+        [bestFitParam, nLL_est] = combineparamfit(model,subjids);
+        AICMat(imodel,isubj,:) = nLL_est + 2*size(bestFitParam,2);
+    end
+    
+    for i = 1:10;
+        count(i,:) = count(i,:) + (AICMat(:,isubj,i) == min(AICMat(:,isubj,i)))';
+    end
+    
+    nParamsVec(imodel) = size(bestFitParam,2);
+end
+
